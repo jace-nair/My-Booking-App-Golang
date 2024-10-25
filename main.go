267502f516c74548b98b475b729a0c831abc4467
Jace <contactjacenair@gmail.com>
2024-10-25 23:02:51 +0100

    Add booking ticket logic

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	// Get the user input
 	fmt.Println("Enter your first name: ")
@@ -32,6 +32,9 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
 }
