ed394b025e73c147174d69d75335ace7e2b4273b
Jace <contactjacenair@gmail.com>
2024-10-27 07:24:59 +0000

    Add condtitional if-else to validate user input

--- main.go
+++ main.go
@@ -7,75 +7,72 @@ import (
 
 func main() {
 
-	//---------------START THE APP--------------------------
-	//var nameOfVariable typeOfData = value
+	const conferenceTickets int = 100
+	var remainingTickets uint = 100
 	var conferenceName string = "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
+	bookings := []string{}
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-
-	//define a slice variable - name of variable & type of value it will hold along with memory allocation.
-	var bookings []string
-	//define a array variable - name of variable & type of value it will hold along with memory allocation.
-	//var bookings []string
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
 
 	for {
-
-		// user name and tickets
 		var firstName string
 		var lastName string
 		var email string
 		var userTickets uint
 
-		//-------------ASKING FOR USER INPUT (SCAN)------------------------
-		// Get the user input
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
+		// asking for user input
+		fmt.Println("Please enter Your First Name: ")
+		fmt.Scanln(&firstName)
 
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
+		fmt.Println("Please enter Your Last Name: ")
+		fmt.Scanln(&lastName)
 
-		fmt.Println("Enter your email address: ")
-		fmt.Scan(&email)
+		fmt.Println("Please enter Your Email: ")
+		fmt.Scanln(&email)
 
-		fmt.Println("Enter number of tickets: ")
-		fmt.Scan(&userTickets)
+		fmt.Println("Please Enter number of tickets: ")
+		fmt.Scanln(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
+		// validate user input
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		//array
-		//bookings[0] = firstName + "" + lastName
+		if isValidName && isValidEmail && isValidTicketNumber {
 
-		//slice
-		bookings = append(bookings, firstName+""+lastName)
+			// book ticket in system
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		//-------------PRINT THE SUMMARY OF BOOKINGS--------------------------
-		fmt.Printf("The whole slice: %v\n", bookings)
-		fmt.Printf("The first value: %v\n", bookings[0])
-		fmt.Printf("Slice type: %T\n", bookings)
-		fmt.Printf("Slice length: %v\n", len(bookings))
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		//-------------EXIT THE APP BY PRINTING SUMMARY MESSAGE FOR THE USER-----------------
+			// print only first names
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names %v\n", firstNames)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-		//Define slice only for First Names
-		firstNames := []string{}
-
-		// Loop through or iterate through bookings list and grab firstName at a time
-		for _, booking := range bookings { // Get the index of the element and the value of the element from the bookings slice. The value will have firstName and lastName with a space character in between.
-			var names = strings.Fields(booking)       // Split booking value string on a space character using the Fields function from strings package. Results in a slice with two elements. Save it as names array.
-			firstNames = append(firstNames, names[0]) //Get the value of element at the index 0 and append it into firstNames slice
+			// exit application if no tickets are left
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("firt name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+			continue
 		}
-
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
-
 	}
 
 }
